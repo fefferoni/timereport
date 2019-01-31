20caad866e83f65fe4745ad91ceb09be9e8f355d
Sebastian Pfeffer <sebastian.pfeffer@cub.se>
2019-01-31 08:53:00 +0100

    Fixed mapping problem Task-TaskModel.

--- TimeReportApi/Models/TaskModel.cs
+++ TimeReportApi/Models/TaskModel.cs
@@ -9,8 +9,8 @@ namespace TimeReport.Web.Api.Models
     public class TaskModel
     {
         public string Name { get; set; }
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
+        public string CreatedByFirstName { get; set; }
+        public string CreatedByLastName { get; set; }
         public string CustomerName { get; set; }
         public string Goal { get; set; }
         public string Background { get; set; }
